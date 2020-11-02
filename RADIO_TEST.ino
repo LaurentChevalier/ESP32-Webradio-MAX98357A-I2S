8481f58510cc84852f150cd7f41b1dba6f8c80ec
Laurent Chevalier <33724778+LaurentChevalier@users.noreply.github.com>
2020-11-02 23:35:17 +0100

    TEST code publish

---
+++ RADIO_TEST.ino
@@ -0,0 +1,76 @@
+/* 
+ *  Code From https://www.xtronical.com/i2sinternetradio/
+ *  */
+
+#include "Arduino.h"
+#include "WiFi.h"
+#include "Audio.h"
+ 
+// Digital I/O used
+#define I2S_DOUT      25  // DIN connection
+#define I2S_BCLK      27  // Bit clock
+#define I2S_LRC       26  // Left Right Clock
+ 
+Audio audio;
+ 
+String ssid =     "Your Wifi SSID";
+String password = "Your Wifi Password";
+ 
+void setup() {
+    //Serial.begin(115200);
+    WiFi.disconnect();
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid.c_str(), password.c_str());
+    while (WiFi.status() != WL_CONNECTED) delay(1500);
+    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+    audio.setVolume(21); // 0...21
+ 
+//    audio.connecttohost("http://www.wdr.de/wdrlive/media/einslive.m3u");
+//    audio.connecttohost("http://macslons-irish-pub-radio.com/media.asx");
+//    audio.connecttohost("http://mp3.ffh.de/radioffh/hqlivestream.aac"); //  128k aac
+     //audio.connecttohost("http://mp3.ffh.de/radioffh/hqlivestream.mp3"); //  128k mp3
+     
+      audio.connecttohost("https://radios.rtbf.be/classic21-128.mp3"); //  128k mp3
+//    audio.connecttospeech("Wenn die Hunde schlafen, kann der Wolf gut Schafe stehlen.", "de");
+//    audio.connecttohost("http://media.ndr.de/download/podcasts/podcast4161/AU-20190404-0844-1700.mp3"); // podcast
+}
+ 
+void loop()
+{
+    audio.loop();
+}
+ 
+// optional
+void audio_info(const char *info){
+    Serial.print("info        "); Serial.println(info);
+}
+void audio_id3data(const char *info){  //id3 metadata
+    Serial.print("id3data     ");Serial.println(info);
+}
+void audio_eof_mp3(const char *info){  //end of file
+    Serial.print("eof_mp3     ");Serial.println(info);
+}
+void audio_showstation(const char *info){
+    Serial.print("station     ");Serial.println(info);
+}
+void audio_showstreaminfo(const char *info){
+    Serial.print("streaminfo  ");Serial.println(info);
+}
+void audio_showstreamtitle(const char *info){
+    Serial.print("streamtitle ");Serial.println(info);
+}
+void audio_bitrate(const char *info){
+    Serial.print("bitrate     ");Serial.println(info);
+}
+void audio_commercial(const char *info){  //duration in sec
+    Serial.print("commercial  ");Serial.println(info);
+}
+void audio_icyurl(const char *info){  //homepage
+    Serial.print("icyurl      ");Serial.println(info);
+}
+void audio_lasthost(const char *info){  //stream URL played
+    Serial.print("lasthost    ");Serial.println(info);
+}
+void audio_eof_speech(const char *info){
+    Serial.print("eof_speech  ");Serial.println(info);
+}
